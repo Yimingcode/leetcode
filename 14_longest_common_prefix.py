417bce0a5e2e2b4d62044b50f8d73adc283584a5
徐一鸣 <xuyiming@xuyimingdeMacBook.local>
2019-05-24 20:43:38 +0800

    14

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -1,6 +1,6 @@
-#/usr/local/bin/python3.6
+# /usr/local/bin/python3.6
 
-##reduce function
+# reduce function
 from functools import reduce
 
 
@@ -25,7 +25,7 @@ class Solution:
         return reduce(self.get_common_prefix, strs)
 
 
-def long_common(strs:[str]):
+def long_common(strs):
     if not strs:
         return ''
 
@@ -49,3 +49,98 @@ def long_common(strs:[str]):
 
 
 # print(long_common(['flow', 'flosedfe', 'flo0000']))
+
+#对于每组数据，输出一个整数，代表最少需要删除的字符个数
+def delete_num():
+    n = input()
+    for i in range(int(n)):
+        i = input()
+        num = 0
+
+
+#输出中的大写字母后移
+
+#n个数 两两数组 第一个数最小差数组个数 最大差数组个数
+# import sys
+# for line in sys.stdin:
+#     a = line.split()
+#     print(int(a[0] + int(a[1])))
+
+def max_min_v():
+    line = input().split()
+    n = int(line[0])
+    s = int(line[1])
+    line2 = input().split()
+    sumi = 0
+    min_v_s = 0
+    max_v = int(line2[0])
+    min_v = int(line2[0])
+    sum2 = 0
+    for i in line2:
+        sumi += int(i)
+    if sumi < s:
+        print(-1)
+    if sumi == s:
+        print(min_v_s)
+
+    if sumi > s:
+        for i in line2:
+            if int(i) <= min_v:
+                min_v = int(i)
+        print(min_v)
+        for i in line2:
+            if int(i) > max_v:
+                max_v = int(i)
+        print(max_v)
+        for i in line2:
+            sum2 += (int(i) - min_v)
+        print(sum2)
+        if sum2 >= s:
+            min_v_s = min_v
+            print(min_v_s)
+            return min_v_s
+        elif max_v - min_v < s:
+            min_v_s = (sumi - s) // n
+            print(min_v_s)
+            return min_v_s
+
+# max_min_v()
+
+def longest_sub():
+    usedChar = {}
+    s = input()
+    start = maxLength = 0
+    for i in range(len(s)):
+
+        if s[i] in usedChar and start <= usedChar[s[i]]:
+            start = usedChar[s[i]] + 1
+            print(usedChar)
+        else:
+            maxLength = max(maxLength, i - start + 1)
+        usedChar[s[i]] = i
+    print(maxLength)
+
+class Solution:
+    def longest_sub2(self):
+        s = input()
+        n = len(s)
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if self.is_unique(s, i, j):
+                    ans = max(ans, j - i)
+        print(ans)
+        return ans
+
+    def is_unique(self, s, start, end):
+        res = 0
+        for n in range(start, end):
+            if s[n] not in s[start:n] and s[n] not in s[n+1:end]:
+                res += 0
+            else:
+                res += 1
+        if res == 0:
+            return True
+        else:
+            return False
+
